7a84d06d1486c5d5fa4416151821c4adb3e6942f
Alexander <kiryanenkoav@mail.ru>
2018-09-22 18:00:24 +0300

    Add swagger

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, permissions, status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from api.serializers import UserSerializer, ProfileSerializer
 
@@ -11,19 +12,36 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class Profile(APIView):
+class Profile(GenericAPIView):
+    serializer_class = ProfileSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    @swagger_auto_schema(
+        operation_id='current_profile',
+        operation_summary='Профиль текущего юзера',
+        request_body=None,
+        responses={
+            status.HTTP_200_OK: ProfileSerializer(many=False),
+            status.HTTP_401_UNAUTHORIZED: 'UNAUTHORIZED'
+        }
+    )
     def get(self, request):
-        """
-        Профиль текущего юзера
-        """
+        """ Профиль текущего юзера """
         serializer = ProfileSerializer(request.user.profile)
         return Response(serializer.data)
 
-    def put(self, request):
+    @swagger_auto_schema(
+        operation_summary='Частичное обновление профиля текущего юзера',
+        responses={
+            status.HTTP_200_OK: ProfileSerializer(many=False),
+            status.HTTP_400_BAD_REQUEST: 'BAD REQUEST',
+            status.HTTP_401_UNAUTHORIZED: 'UNAUTHORIZED'
+        }
+    )
+    def patch(self, request):
         """
         Обновить поля профиля текущего юзера
+        Обновление частичное (не требуется заполнение всех полей)
         """
         serializer = ProfileSerializer(request.user.profile, data=request.data)
 
--- keklik/urls.py
+++ keklik/urls.py
@@ -15,10 +15,31 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework.documentation import include_docs_urls
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Keklik API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kiryanenkoav@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   validators=['flex', 'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-    url(r'^', include_docs_urls(title='Keklik API')),
+    url(r'^docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
