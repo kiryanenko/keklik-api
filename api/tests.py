2df02463d7689f9d5d7f8c79e823112200fabf02
Alexander <kiryanenkoav@mail.ru>
2018-10-07 01:22:18 +0300

    Add test get scheme

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SchemeTestCase(TestCase):
+    def testGetScheme(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
