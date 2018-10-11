24256b475632eb2755268a5a4a9e6602410706b3
Alexander <kiryanenkoav@mail.ru>
2018-10-12 02:11:23 +0300

    Add user quizzes

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from django.urls import include
 from rest_framework import routers
 
-from api.views.quiz import QuizViewSet
-from api.views.user import UserViewSet, SessionView, CurrentUserView, PasswordView
+from api.views.quizzes import QuizViewSet, UserQuizzesView, CurrentUserQuizzesView
+from api.views.users import UserViewSet, SessionView, CurrentUserView, PasswordView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,5 +13,7 @@ urlpatterns = [
     url(r'^session/$', SessionView.as_view()),
     url(r'^users/me/$', CurrentUserView.as_view()),
     url(r'^users/me/password/$', PasswordView.as_view()),
+    url(r'^users/me/quizzes/$', CurrentUserQuizzesView.as_view()),
+    url(r'^users/(?P<username>\w+)/quizzes/$', UserQuizzesView.as_view()),
     url(r'^', include(router.urls)),
 ]
---
+++ api/views/quizzes.py
@@ -0,0 +1,41 @@
+from rest_framework import permissions, filters
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+
+from api.models import Quiz
+from api.serializers import QuizSerializer
+
+
+class QuizViewSet(ModelViewSet):
+    queryset = Quiz.objects.filter(old_version__isnull=True)
+    serializer_class = QuizSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('id', 'version_date', 'title', 'rating')
+    ordering = ('-rating', '-version_date', '-id')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class UserQuizzesView(ListAPIView):
+    serializer_class = QuizSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('id', 'version_date', 'title', 'rating')
+    ordering = ('-rating', '-version_date', '-id')
+
+    def get_queryset(self):
+        username = self.kwargs['username']
+        return Quiz.objects.filter(user__username=username, old_version__isnull=True)
+
+
+class CurrentUserQuizzesView(UserQuizzesView):
+    permission_classes = (permissions.IsAuthenticated,)
+    ordering = ('-version_date', '-id')
+
+    def get_queryset(self):
+        user = self.request.user
+        return Quiz.objects.filter(user__username=user, old_version__isnull=True)
