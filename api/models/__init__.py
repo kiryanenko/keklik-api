571387aec18c2370066ded5584ece5f72eef5356
Alexander <kiryanenkoav@mail.ru>
2018-10-14 03:14:17 +0300

    Add game models

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,11 @@
 from api.models.user import User
+
 from api.models.quiz import Quiz
 from api.models.quiz import Question
 from api.models.quiz import Variant
 from api.models.quiz import Tag
+
+from api.models.game import Game
+from api.models.game import GeneratedQuestion
+from api.models.game import Player
+from api.models.game import Answer
