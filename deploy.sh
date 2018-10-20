941e5e4ced6d60ecec807cab716a6265f470a529
Alexander <kiryanenkoav@gmail.com>
2018-10-20 14:25:47 +0000

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ pkill python
 
 pip3 install -r requirements.txt
 python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py collectstatic -c --no-input
 
 daphne keklik.asgi:channel_layer -b 0.0.0.0 -p 8000 -v2 &
 python3 manage.py runworker -v2 &
