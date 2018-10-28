4719a754805ec44324f2c4c0a833ed145c00c82b
Alexander <kiryanenkoav@mail.ru>
2018-10-29 01:57:14 +0300

    Add virtualenv

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ stderr=logs/deploy.log
 pkill daphne
 pkill python
 
+source ./env/bin/activate
+
 pip3 install -r requirements.txt > ${stdout} 2> ${stderr}
 python3 manage.py migrate >> ${stdout} 2>> ${stderr}
 python3 manage.py collectstatic -c --no-input >> ${stdout} 2>> ${stderr}
