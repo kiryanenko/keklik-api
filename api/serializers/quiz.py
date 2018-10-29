8fcf5bcb5824af487a2ae3dd3e4f50e9d9a0b433
Alexander <kiryanenkoav@mail.ru>
2018-10-30 02:48:41 +0300

    Add validation for answer

--- api/serializers/quiz.py
+++ api/serializers/quiz.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from api.models import Variant, Question, Tag, Quiz
 from api.serializers.user import UserSerializer
@@ -18,6 +19,16 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ('id', 'number', 'type', 'question', 'variants', 'answer', 'timer', 'points')
         read_only_fields = ('number',)
+
+    def validate(self, attrs):
+        answer = attrs.get('answer')
+        variants = attrs.get('variants')
+
+        for ans in answer:
+            if ans > len(variants) or ans < 1:
+                raise ValidationError(detail='Answer elements should be in 1..count_of_variants.')
+
+        return attrs
 
 
 class QuizSerializer(serializers.ModelSerializer):
